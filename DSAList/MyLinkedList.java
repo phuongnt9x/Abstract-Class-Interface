25fec4a720209c4854c1b385b15aa0ce84deabbc
Nguyen Phuong <95663567+phuongnt9x@users.noreply.github.com>
2022-01-13 14:44:22 +0700

    LinkedList

--- DSAList/MyLinkedList.java
+++ DSAList/MyLinkedList.java
@@ -2,8 +2,9 @@ package DSAList;
 
 public class MyLinkedList {
 	private  Node head;
-    private  int numNodes;
-
+    private  int numNodes=0;
+    public MyLinkedList() {
+    }
     public MyLinkedList(Object data) {
         head = new Node(data);
     }
@@ -38,6 +39,26 @@ public class MyLinkedList {
         head.next = temp;
         numNodes++;
     }
+    public void addLast(Object data) {
+    	Node temp = head;
+        while(temp.next!=null) {
+        	temp=temp.next;
+        }
+        temp.next = new Node(data);
+        temp.next.next = null;
+        numNodes++;
+    }
+    public void remove(int index) {
+        Node temp = head;
+        Node holder;
+
+        for(int i=0; i < index-2 && temp.next != null; i++) {
+            temp = temp.next;
+        }
+       holder = temp.next.next;
+        temp.next = holder;
+        numNodes--;
+    }
     public Node get(int index){
         Node temp=head;
         for(int i=0; i<index; i++) {
@@ -52,5 +73,48 @@ public class MyLinkedList {
             temp = temp.next;
         }
     } 
+    public int size() {
+    	return numNodes;
+    }
+    public void clear() {
+        for (Node x = head; x != null; ) {
+            Node next = x.next;
+            x.data = null;
+            x.next = null;
+            x = next;
+        }
+        head = null;
+        numNodes = 0;
+    }
+    public Node getFirst(){
+        return head;
+    }
+    public Node getLast(){
+        Node temp=head;
+        while(temp.next!=null) {
+        	temp=temp.next;
+        }
+        return temp;
+    }
+    public int indexOf(Object o) {
+        int index = 0;
+        if (o == null) {
+            for (Node x = head; x != null; x = x.next) {
+                if (x.data == null)
+                    return index;
+                index++;
+            }
+        } else {
+            for (Node x = head; x != null; x = x.next) {
+                if (o.equals(x.data))
+                    return index;
+                index++;
+            }
+        }
+        return -1;
+    }
+    public boolean contains(Object o) {
+        return indexOf(o) != -1;
+    }
 
 }
